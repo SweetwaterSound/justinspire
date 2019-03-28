cb2881b37e8b1741f115561c0b02811fb4c7f1c2
Michael R. Shelton <me@michaelrshelton.com>
2019-03-28 19:56:09 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function justInspire()
+{
+    const words = {
+        verbs: [],
+        greater: [],
+        object: [],
+        lesser: [],
+        with: [],
+    };
+    
+    
+    // return words[whichWord][Math.floor(Math.random() * words[whichWord].length)];
+    
+    return "";
+}
