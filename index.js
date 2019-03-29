370308fa373c823608e58c3402d3611caff87e40
Michael Shelton <michael_shelton@sweetwater.com>
2019-03-28 22:46:45 -0400

    Updated the inspiration response from sample to simple return text.

--- index.js
+++ index.js
@@ -2,53 +2,23 @@
 
 const inspiration = require('./TheMindOfAGustin');
 
-function formatSlackMessage(query, response) {
-    let entity;
-    if (
-        response &&
-        response.data &&
-        response.data.itemListElement &&
-        response.data.itemListElement.length > 0
-    ) {
-        entity = response.data.itemListElement[0].result;
-    }
-
-    // https://api.slack.com/docs/message-formatting
+/**
+ *  https://api.slack.com/docs/message-formatting
+ * @param predicate
+ * @param message
+ * @returns {number}
+ */
+function formatSlackMessage(message) {
     const slackMessage = {
         response_type: 'in_channel',
-        text: `Query: ${query}`,
+        text: message,
         attachments: [],
     };
 
-    if (! entity) {
-        return slackMessage.attachments.push({text: '...'});
-    }
-
-    const attachment = { color: '#3367d6' };
-
-    if (entity.name) {
-        attachment.title = entity.name;
-        if (entity.description) {
-            attachment.title = `${attachment.title}: ${entity.description}`;
-        }
-    }
-
-    if (entity.detailedDescription) {
-        if (entity.detailedDescription.url) {
-            attachment.title_link = entity.detailedDescription.url;
-        }
-        if (entity.detailedDescription.articleBody) {
-            attachment.text = entity.detailedDescription.articleBody;
-        }
-    }
-
-    if (entity.image && entity.image.contentUrl) {
-        attachment.image_url = entity.image.contentUrl;
-    }
-
-    slackMessage.attachments.push(attachment);
-
-    return slackMessage;
+    // formatting goes here
+    return slackMessage.attachments.push({
+        text: message,
+    });
 }
 
 function verifyWebhook(body) {
@@ -61,7 +31,7 @@ function verifyWebhook(body) {
 
 function justInspire(predicate = "") {
     return new Promise((resolve, reject) => {
-        resolve(formatSlackMessage(predicate, inspiration.say(predicate)));
+        resolve(formatSlackMessage(inspiration.say(predicate.trim())));
     });
 }
 
